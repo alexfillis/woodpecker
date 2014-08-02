5312d226bcbad9f7230670225f612da8453839c0
Alex Fillis <alexfillis@gmail.com>
2014-08-02 10:45:48 +0800

    Added code and config to run up a webserver for serving up Kibana.

---
+++ src/main/java/woodpecker/MainKibana.java
@@ -0,0 +1,65 @@
+package woodpecker;
+
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.jvm.*;
+import org.elasticsearch.metrics.ElasticsearchReporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.webbitserver.WebServer;
+import org.webbitserver.WebServers;
+import org.webbitserver.handler.StaticFileHandler;
+
+import java.io.IOException;
+import java.lang.management.ManagementFactory;
+import java.net.InetAddress;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import static com.codahale.metrics.MetricRegistry.name;
+import static java.lang.String.format;
+import static java.util.concurrent.TimeUnit.*;
+
+public class MainKibana {
+    private final Logger logger = LoggerFactory.getLogger(getClass());
+
+    public static void main(String[] args) {
+        new MainKibana().run(args);
+    }
+
+    private void run(String[] args) {
+        try {
+            String[] configFilePath = args[0].split(";");
+            Configuration config = new Configuration(configFilePath);
+
+            final MetricRegistry registry = new MetricRegistry();
+            registry.register(name(getClass(), "buffers"), new BufferPoolMetricSet(ManagementFactory.getPlatformMBeanServer()));
+            registry.register(name(getClass(), "files"), new FileDescriptorRatioGauge());
+            registry.register(name(getClass(), "garbage"), new GarbageCollectorMetricSet());
+            registry.register(name(getClass(), "memory"), new MemoryUsageGaugeSet());
+            registry.register(name(getClass(), "threads"), new ThreadStatesGaugeSet());
+            final ConsoleReporter reporter = ConsoleReporter.forRegistry(registry)
+                    .convertRatesTo(SECONDS)
+                    .convertDurationsTo(MILLISECONDS)
+                    .build();
+            reporter.start(1, MINUTES);
+            ElasticsearchReporter esReporter = ElasticsearchReporter.forRegistry(registry)
+                    .hosts("localhost:9200", "localhost:9201")
+                    .prefixedWith(format("%s:%d", InetAddress.getLocalHost().getHostName(), config.getInt("http.port", 8080)))
+                    .convertRatesTo(SECONDS)
+                    .convertDurationsTo(MILLISECONDS)
+                    .build();
+            esReporter.start(1, MINUTES);
+
+            WebServer webServer = WebServers.createWebServer(config.getInt("http.port", 8080))
+                    .add(new StaticFileHandler("/home/alex/apps/kibana-3.1.0"))
+                    .uncaughtExceptionHandler(new WebServerUncaughtExceptionHandler(registry));
+            Future future = webServer.start();
+            future.get();
+            logger.info("Listening on {}", webServer.getUri());
+        } catch (InterruptedException | ExecutionException | IOException e) {
+            logger.error(e.getMessage(), e);
+            System.exit(1);
+        }
+    }
+}

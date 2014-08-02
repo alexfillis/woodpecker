345b82e41111d27dfee513fd67c2aee5a9039bbf
Alex Fillis <alexfillis@gmail.com>
2014-08-02 15:20:36 +0800

    Added timing metrics to handler wrapper.

--- src/main/java/woodpecker/CounterHandlerWrapper.java
+++ src/main/java/woodpecker/CounterHandlerWrapper.java
@@ -2,6 +2,7 @@ package woodpecker;
 
 import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
+import com.codahale.metrics.Timer;
 import org.webbitserver.HttpControl;
 import org.webbitserver.HttpHandler;
 import org.webbitserver.HttpRequest;
@@ -14,20 +15,28 @@ public class CounterHandlerWrapper implements HttpHandler {
     private final HttpHandler handler;
     private final Counter successCounter;
     private final Counter failCounter;
+    private final Timer requestTimer;
 
     public CounterHandlerWrapper(HttpHandler handler, MetricRegistry metricRegistry) {
         this.handler = handler;
         successCounter = metricRegistry.counter(name(handler.getClass(), "request", "success"));
         failCounter = metricRegistry.counter(name(handler.getClass(), "request", "fail"));
+        requestTimer = metricRegistry.timer(name(handler.getClass(), "request", "time"));
     }
 
     @Override
     public void handleHttpRequest(HttpRequest request, HttpResponse response, HttpControl control) throws Exception {
+        Timer.Context time = requestTimer.time();
         try {
-            handler.handleHttpRequest(request, response, control);
-            successCounter.inc();
-        } catch (Exception e) {
-            failCounter.inc();
+            try {
+                handler.handleHttpRequest(request, response, control);
+                successCounter.inc();
+            } catch (Exception e) {
+                failCounter.inc();
+                throw e;
+            }
+        } finally {
+            time.stop();
         }
     }
 }
